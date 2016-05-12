0368fafcc20e88753c7e28cdf8eedd1188351471
Ziv Zamechek <ziv921@yahoo.com>
2016-05-12 01:41:02 -0400

    need to debug the images of homepage and images of user profile do not appear

--- db/migrate/20160511152009_create_friendships.rb
+++ db/migrate/20160511152009_create_friendships.rb
@@ -3,7 +3,7 @@ class CreateFriendships < ActiveRecord::Migration
     create_table :friendships do |t|
       t.integer :user_id
       t.integer :friend_id
-      t.string :state
+      t.string :state, default: "pending"
       t.datetime :friended_at
 
       t.timestamps null: false
